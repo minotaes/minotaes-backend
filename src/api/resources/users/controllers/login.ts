633e840c91c3a0ac20a7a0200e36ca239c45e3f8
diegoleondev <diegoleon.dev@gmail.com>
2024-09-02 00:40:13 -0600

    user controller demo created

---
+++ src/api/resources/users/controllers/login.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+import {
+  passwordSchema,
+  validateSchema,
+} from "../../../../lib/schema/index.js";
+import { NotFoundError } from "../../../../lib/http-error/index.js";
+import { isNullish } from "../../../../utils/check.js";
+import { controllerHandler } from "../../../middleware/index.js";
+
+const sh = z.object({
+  body: z.object({
+    email: z.string().email(),
+    password: passwordSchema,
+  }),
+});
+
+export const loginController = controllerHandler(
+  {
+    test: validateSchema(sh),
+  },
+  async ({ attrs, deps }) => {
+    const user = await deps.models.user.findOne({
+      where: {
+        email: attrs.body.email,
+      },
+      attributes: ["password"],
+    });
+
+    if (isNullish(user)) {
+      throw new NotFoundError({
+        message: `Email ${attrs.body.email} not found.`,
+        details: {},
+      });
+    }
+
+    return {
+      status: 200,
+      body: {
+        message: "Login exitoso",
+      },
+    };
+  },
+);
