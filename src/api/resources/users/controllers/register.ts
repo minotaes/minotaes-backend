b3e249c225a3b2c508e0f27b038f78d270e9f96e
diegoleondev <diegoleon.dev@gmail.com>
2024-10-01 15:25:55 -0600

    added email verification

--- src/api/resources/users/controllers/register.ts
+++ src/api/resources/users/controllers/register.ts
@@ -6,15 +6,18 @@ import bcrypt from "bcrypt";
 
 import { controllerHandler } from "#api/middleware/index.js";
 import {
-  emailSchema,
   passwordSchema,
+  stringSchema,
   validateSchema,
 } from "#lib/schema/index.js";
 import { ENV } from "#config/env.js";
+import { isNullish } from "#root/utils/check.js";
+import { DETAILS } from "#root/constants/details.js";
+import { BadRequestError } from "#root/lib/http-error/index.js";
 
 const requestSchema = z.object({
   body: z.object({
-    email: emailSchema,
+    token: stringSchema,
     password: passwordSchema,
   }),
 });
@@ -24,11 +27,29 @@ export const registerController = controllerHandler(
     test: validateSchema(requestSchema),
   },
   async ({ attrs, deps }) => {
+    let email: string | undefined;
+
+    try {
+      const decoded = jwt.verify(attrs.body.token, ENV.SERVER.JWT) as {
+        e: string;
+      };
+      email = decoded.e;
+    } catch (error) {
+      console.error(error);
+    }
+
+    if (isNullish(email)) {
+      throw new BadRequestError({
+        message: "Token is invalid or expired",
+        details: { token: DETAILS.INVALID },
+      });
+    }
+
     const userId = crypto.randomUUID();
 
     await deps.models.user.create({
       userId,
-      email: attrs.body.email,
+      email,
     });
 
     const token = jwt.sign({ u: userId }, ENV.SERVER.JWT, {
